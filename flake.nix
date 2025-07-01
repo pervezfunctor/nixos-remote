b579ff4be8996cc7987f0005471104c809eceec4
Pervez Iqbal <pervefunctor@gmail.com>
2025-07-01 23:08:17 +0530

    fixes

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05"; };
 
   outputs = { self, nixpkgs }: {
-    nixosConfigurations.nixos-remote-unlock = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.ilm = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [ ./configuration.nix ];
     };
